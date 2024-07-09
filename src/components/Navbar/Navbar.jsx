c7f7ff57dfc547702b2b7f697a20817b6c8e575d
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-07-09 16:59:31 +0530

    navbar implemeted

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,15 +5,15 @@ import logo from '../../assets/logo.png';
 function Navbar() {
   return (
     <div>
-        <nav>
-          <img src={logo}></img>
+        <nav className="container">
+          <img className="logo" src={logo}></img>
           <ul>
             <li>Home</li>
             <li>Program</li>
             <li>About us</li>
             <li>Campus</li>
             <li>Testimonials</li>
-            <li>Contact Us</li>
+            <li><button className="btn">Contact Us</button></li>
           </ul>
         </nav>
     </div>
