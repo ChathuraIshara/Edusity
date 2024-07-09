716734512f84cf0a0121067bedc32860b3ceb74e
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-07-09 19:32:10 +0530

    Hero text implemented

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Hero.css';
+import darkarrow from '../../assets/dark-arrow.png';
 
 function Hero() {
   return (
-    <div className="hero">
-        <h1>Hello</h1>
+    <div className="hero container">
+        <div className="herotext">
+            <h1>We ensure better education for a better world</h1>
+            <p>our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
+            <button className='btn'>Explore More <img src={darkarrow}></img></button>
+        </div>
     </div>
   )
 }
